bc87a1b27367d729885c903e8887013b12af0f67
Ryan <leehahnwoong@gmail.com>
2023-03-10 22:21:12 -0800

    [3/10] leetcode 409

---
+++ python/week1/409.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        occurs = {}
+        length = 0
+        odds = False
+        for i in s:
+            if i in occurs:
+                occurs[i] += 1
+            else:
+                occurs[i] = 1
+        for key,val in occurs.items():
+            if val % 2 == 0:
+                length += val
+            else:
+                odds = True
+                if val > 1:
+                    length += (val -1)
+        if odds:
+            length += 1
+        return length
