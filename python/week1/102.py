c710e8b42a16941ea9a307428c90bfbe4a504580
Ryan <leehahnwoong@gmail.com>
2023-03-12 21:15:25 -0700

    [3/12] leetcode 102

---
+++ python/week1/102.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return root
+        queue = [root]
+        result = []
+        while queue:
+            level = []
+            length = len(queue)
+            for i in range(length):
+                current = queue.pop(0)
+                level.append(current.val)
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+            result.append(level)
+        return result
