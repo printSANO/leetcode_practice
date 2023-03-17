858488016328cacf046e76d13afefbd2b3893573
Ryan <leehahnwoong@gmail.com>
2023-03-16 22:27:37 -0700

    [3/16] leetcode 509

---
+++ python/week2/509.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        f = [0]*(n+1)
+        if n < 2:
+            return n
+        f[0] = 0
+        f[1] = 1
+        for i in range(2,n+1):
+            f[i] = f[i-1] + f[i-2]
+        return f[n]
