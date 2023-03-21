784b31e11cf73ffd8acea541f9d64856e1822ec6
Ryan <leehahnwoong@gmail.com>
2023-03-21 13:42:02 -0700

    [3/21] leetcode 28 add another solution

--- python/week3/28.py
+++ python/week3/28.py
@@ -5,3 +5,11 @@ class Solution:
         if x is None:
             return -1
         return x.span()[0]
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            x = haystack.index(needle)
+            return x
+        except:
+            return -1
