8bb35be0b9edc9693251401ef84818f31e780f75
Ryan <leehahnwoong@gmail.com>
2023-04-06 14:43:16 -0700

    [4/6] leetcode 1299

--- python/week5/1299.py
+++ python/week5/1299.py
@@ -1,1 +1,14 @@
-#solve backwards
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        arr += [-1]
+        for i in range(len(arr)-2,0,-1):
+            arr[i] = max(arr[i], arr[i+1])
+        return arr[1:]
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1,1,-1):
+            if arr[i-1] < arr[i]:
+                arr[i-1] = arr[i]
+        arr = arr[1:] + [-1]
+        return arr
