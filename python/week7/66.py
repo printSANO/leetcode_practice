30a20bfd7f8ef0907730f17a3301bd2750ddb174
Ryan <leehahnwoong@gmail.com>
2023-04-17 17:59:47 -0700

    [4/17] leetcode 66

---
+++ python/week7/66.py
@@ -0,0 +1,5 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = [str(i) for i in digits]
+        digits = int("".join(digits))+1
+        return [int(i) for i in str(digits)]
