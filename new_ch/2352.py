31c21215f4609b5366bcfe91ab4d34d0ce167a4b
Ryan <leehahnwoong@gmail.com>
2023-09-22 21:53:06 -0700

    [9/22] leetcode 2352.py

---
+++ new_ch/2352.py
@@ -0,0 +1,7 @@
+import numpy
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        rows = Counter(map(tuple, grid))
+        cols = Counter(map(tuple, zip(*grid)))
+        return sum(v * cols.get(k, 0) for k, v in rows.items())
+        
